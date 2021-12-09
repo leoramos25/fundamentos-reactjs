bc16c259de645e3a4e8a761faf49d6c8205b39c8
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-12-09 14:38:52 -0300

    Add fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactWebpackRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -15,19 +16,29 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    static: path.resolve(__dirname, 'public', 'index.html')
+    static: path.resolve(__dirname, 'public', 'index.html'),
+    hot: true
   },
   plugins: [
-    new htmlWebpackPlugin({
+    isDevelopment && new ReactWebpackRefreshPlugin(),
+    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+
+        }
       },
       {
         test: /\.scss$/,
