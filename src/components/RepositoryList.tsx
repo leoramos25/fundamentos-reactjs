6ba081f6fc587ed5485e3b17141b47fb1eb64695
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-12-10 09:36:50 -0300

    Refactoring for typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,15 +2,21 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+type Repository = {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/leoramos25/repos')
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
-  
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
